a885e9f05eb00cc59681797f0d00f8bba37b4bf6
lim950914 <dlackswn2222@naver.com>
2021-03-30 16:04:58 +0900

    0330

--- src/p04/textbook/IfNestedExample.java
+++ src/p04/textbook/IfNestedExample.java
@@ -1,7 +1,27 @@
 package p04.textbook;
 
 public class IfNestedExample {
-	int score = (int) (Math.random() * 20) + 81;
-	
+	public static void main(String[] args) {
+		// 밑에서 20은 0 1 2 ..... 18 19
+		int score = (int) (Math.random() * 20) + 81;
+		System.out.println("점수: " + score);
+		
+		String grade;
+		
+		if (score >= 90) {
+			if (score >= 95) {
+				grade = "A+";
+			} else {
+				grade = "A";
+			}
+		} else {
+			if (score >= 85) {
+				grade = "B+";
+			} else {
+				grade = "B";
+			}
+		}
+		
+		System.out.println("학점: " + grade);
 	}
 }
