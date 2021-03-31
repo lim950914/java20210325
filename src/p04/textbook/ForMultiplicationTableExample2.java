4e766771d0d86dba29b27bd5e9b785dc7c667ce2
lim950914 <dlackswn2222@naver.com>
2021-03-31 16:55:09 +0900

    0331 PM

--- src/p04/textbook/ForMultiplicationTableExample2.java
+++ src/p04/textbook/ForMultiplicationTableExample2.java
@@ -8,43 +8,73 @@ public class ForMultiplicationTableExample2 {
 		System.out.print("같은 줄");
 		System.out.print("같은 줄");
 		System.out.println("새로운 줄");
-		*/
-		
+		 */
 		/*
 		*
 		**
 		***
-		**** 
-		***** 
-		*/
+		****
+		*****
+		 */
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		
-		 /*
-		*****
-		****
-		***
-		**
-		*
-		*/
-		for (int a = 5; a > 0; a--) {
-			for (int b = 0; b <= a; b++) {
+		/*
+		 *****
+		 ****
+		 ***
+		 **
+		 *
+		 */
+		System.out.println("---------------------");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < (5-i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		/*
+		 *****
+		  ****
+		   ***
+		    **
+		     *
+		 */
+		System.out.println("---------------------");
+		for (int i = 0; i < 5; i++) {
+			for (int k = 0; k < i; k++){
+				System.out.print(" ");
+			}
+			for (int j = 0; j < (5-i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		/*
+		     *
+		    **
+		   ***
+		  ****
+		 *****
+		 */
+		System.out.println("---------------------");
+		for (int i = 0; i < 5; i++) {
+			for (int k = 0; k < (4-i); k++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 		
-		 /*
-		*****
-		 ****
-		  ***
-		   **
-		    * 
-		 */
-		
 	}
 }
+
+
+
+
+
