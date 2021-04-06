061f7102342c4ce915272cc3e28e4920f70da865
lim950914 <dlackswn2222@naver.com>
2021-04-06 15:32:25 +0900

    0406B

---
+++ src/p06/lecture/p1class/Student.java
@@ -0,0 +1,13 @@
+package p06.lecture.p1class;
+
+// 객체를 만들기 위하 설계도 (class)
+
+// class 명 : UpperCamelCase 맨앞은 대문자
+// 숫자, 영문, $, _ 조합: 숫자가 앞에 오면 안됨
+
+// class 키워드가 앞에 옴 밑에 class Student
+
+// class 명 다음에 class 본문 {} 작성
+public class Student {
+
+}
---
+++ src/p06/lecture/p3constructor/TV.java
@@ -0,0 +1,10 @@
+package p06.lecture.p3constructor;
+
+public class TV {
+	// this 키워드
+	String name;
+	
+	TV(String name){
+		this.name = name;
+	}
+}
