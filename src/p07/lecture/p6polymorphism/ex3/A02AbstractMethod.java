a65cebc09c52998db6eced0920b215448ac8ae07
lim950914 <dlackswn2222@naver.com>
2021-04-13 15:14:24 +0900

    0413B

--- src/p07/lecture/p6polymorphism/ex3/A02AbstractMethod.java
+++ src/p07/lecture/p6polymorphism/ex3/A02AbstractMethod.java
@@ -1,10 +1,11 @@
 package p07.lecture.p6polymorphism.ex3;
 
-public class A01AbstractClass {
+public class A02AbstractMethod {
 	public static void main(String[] args) {
 		KindaCat k1 = new Cat();
 		KindaCat k2 = new Tiger();
 		
-//		KindaCat k3 = new KidaCat(); KindaCat class를 Abstract 로 설정해서 새로운 인스턴스 생성불가
+		k1.cry();
+		k2.cry();
 	}
 }
