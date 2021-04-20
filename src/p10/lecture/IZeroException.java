ec701c09708d2909ca63149549d73b9a5a582dc7
lim950914 <dlackswn2222@naver.com>
2021-04-20 14:01:04 +0900

    20210420B

---
+++ src/p10/lecture/IZeroException.java
@@ -0,0 +1,11 @@
+package p10.lecture;
+
+public class IZeroException extends Exception {
+	public IZeroException() {
+		
+	}
+	// Exception 생성
+	public IZeroException(String message) {
+		super(message);
+	}
+}
