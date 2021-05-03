02e638223092bf68bd992d92d2476949bd414eb0
lim950914 <dlackswn2222@naver.com>
2021-05-03 11:18:49 +0900

    20210503A

---
+++ src/p18/lecture/A07FileCopy.java
@@ -0,0 +1,28 @@
+package p18.lecture;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class A07FileCopy {
+	public static void main(String[] args) throws Exception {
+		String src = "src/p18/lecture/A02InputStream.java";
+		String des = "src/p18/lecture/A02InputStream.copy2";
+		
+		InputStream is = new FileInputStream(src);
+		OutputStream os = new FileOutputStream(des);
+		
+		byte[] datas = new byte[20];
+		int len = 0;
+		while ((len = is.read(datas)) != -1) {
+//			os.write(datas); // 무조건 20bytes 사용하지만
+			os.write(datas, 0 , len); // 설정해준만큼 사용함
+		}
+		
+		is.close();
+		os.close();
+		
+		System.out.println("복사 완료");
+	}
+}
